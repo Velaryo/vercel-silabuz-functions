71b5500efd78203e23aa6d40ffe3c16e9e6937bb
Alvaro <ixml3d@gmail.com>
2023-01-25 19:55:01 -0500

    conect repo

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,16 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  // cuando compile, lo use como una function 
-  //y no como una api normal
-  runtime: "edge"
+  runtime: "edge",
 };
-
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req: NextRequest) => {
   return NextResponse.json({
-    message: `Hello world ${req.url}`,
+    ok: true,
+    data: [
+      {
+        id: 1,
+        name: "Linder",
+      },
+      {
+        id: 2,
+        name: "Lucas",
+      },
+    ],
   });
 };
-
